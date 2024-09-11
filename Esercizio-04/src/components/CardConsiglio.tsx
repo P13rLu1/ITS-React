671fe40560c2d5c605fde0f9d37e6195db52d748
P13rLu1 <pbuttazzo@studenti.apuliadigitalmaker.it>
2024-09-11 10:07:14 +0200

    added new exercise

--- Esercizio-04/src/components/CardConsiglio.tsx
+++ Esercizio-04/src/components/CardConsiglio.tsx
@@ -5,7 +5,7 @@ interface Props {
 function CardConsiglio({consiglio}: Props) {
 
     return (
-        <div style={{border: "1px solid gray", borderRadius: 25, marginTop: 20, padding: 10, backgroundColor: "#1a1a1a"}}>
+        <div style={{border: "1px solid gray", borderRadius: 25, marginTop: 20, padding: 20, backgroundColor: "#1a1a1a"}}>
             <h4>{consiglio}</h4>
         </div>
     );
